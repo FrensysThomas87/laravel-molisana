d334b8071b5b21e698db1ad45be7a5234f535cc4
FrensysThomas87 <delta873@gmail.com>
2021-03-11 23:52:00 +0100

    Creato partial del footer

--- resources/views/pages/home-page.blade.php
+++ resources/views/pages/home-page.blade.php
@@ -188,61 +188,10 @@ $array = json_decode($data, true);
       {{-- end main --}}
 
       {{-- footer --}}
-      <footer>
-        <div class="footer-contacts">
-          <img src="https://www.lamolisana.it/wp-content/uploads/2020/04/marchio-sito-test.png" alt="logo-molisana">
-          <p>RAGIONE SOCIALE: LA MOLISANA S.P.A.</p>
-              <p>SEDE LEGALE: CONTRADA COLLE DELLE API, 100/A<p>
-              <p>PEC: LAMOLISANA@PEC.IT</p>
-              <p>TEL: +39 0874 4981</p>
-              <p>FAX: +39 0874 629584</p>
-              <p>PER SEGNALAZIONI SCRIVERE A:</p>
-              <p>INFO@LAMOLISANA.IT</p>
-              <p>PER CONTATTARE L'AREA COMMERCIALE:</p>
-              <p>COMMERCIALE@LAMOLISANA.IT</p>
-              <p>PER CONTATTARE L'AREA EXPORT:</p>
-              <p>EXPORT@LAMOLISANA.IT</p>
-              <p>DATI AZIENDALI: LEGGI DI PIÙ</p>
-        </div>
-
-        <div class="footer-element-container">
-          <div class="footer-element">
-            <h2>PASTIFICIO</h2>
-            <li>Il pastificio</li>
-            <li>Grano decorticato a pietra</li>
-            <li>Il molise c'è</li>
-            <li>Filiera integrata</li>
-            <li>100 anni di pasta</li>
-            <li>Sartoria della pasta</li>
-            <li>Spaghetto Quadrato</li>
-            <li>Le Persone</li>
-            <h2>PRODOTTI</h2>
-            <li>Le Classiche</li>
-            <li>Le Integrali</li>
-            <li>Le Speciali</li>
-            <li>Le Biologiche</li>
-            <li>Le Gluten-Free</li>
-            <li>Le Semole</li>
-            <li>Le Extra di Lusso</li>
-          </div>
-
-          <div class="footer-element">
-            <h2>COLLEZIONE DA CHEF</h2>
-            <li>Collezione da Chef</li>
-            <li>Grandi Cucine</li>
-            <li>Biologiche</li>
-            <li>Quadrate</li>
-          </div>
-        </div>
-        <div class="footer-element">
-      </div>
-
-  
-
-
-
-      </footer>
+      @include('partials.footer')
       {{-- end footer --}}
+
+
 
   </body>
 </html>
