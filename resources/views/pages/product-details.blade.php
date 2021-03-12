057fb4216d5f8bd6e665b9e2f2139314a4f41443
FrensysThomas87 <delta873@gmail.com>
2021-03-12 15:23:00 +0100

    Impostata immagine per tipo di pasta richiamato

--- resources/views/pages/product-details.blade.php
+++ resources/views/pages/product-details.blade.php
@@ -1,5 +1,29 @@
 
 
-questa è la pagina prodotto {{ $idProduct }}
+{{-- questa è la pagina prodotto {{ $idProduct }}
 
-{{ $pastaArray[$idProduct - 1]['descrizione'] }}
+{{ $pastaArray[$idProduct - 1]['descrizione'] }} --}}
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+  </head>
+  <body>
+    {{-- header --}}
+    @include('partials.header')
+    {{--end header --}}
+      <main id="product-main">
+        <img src={{$pastaArray[$idProduct - 1]['src-h']}} alt="">
+        <img src={{$pastaArray[$idProduct - 1]['src-p']}} alt="">
+      </main>
+
+
+
+    {{-- footer --}}
+    @include('partials.footer')
+    {{-- end footer --}}
+  </body>
+</html>
