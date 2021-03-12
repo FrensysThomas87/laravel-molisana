684056de5563433b2876caf5f4a2e604ab46ef90
FrensysThomas87 <delta873@gmail.com>
2021-03-12 15:27:25 +0100

    Impostato titolo prodotto

--- resources/views/pages/product-details.blade.php
+++ resources/views/pages/product-details.blade.php
@@ -16,6 +16,7 @@
     @include('partials.header')
     {{--end header --}}
       <main id="product-main">
+        <h1>{{$pastaArray[$idProduct - 1]['titolo']}}</h1>
         <img src={{$pastaArray[$idProduct - 1]['src-h']}} alt="">
         <img src={{$pastaArray[$idProduct - 1]['src-p']}} alt="">
       </main>
